ecdb13a3071dd90b0429ce151d784f963ec5fe02
wupanhao <877594836@qq.com>
2016-04-02 19:51:42 +0800

    configuration

--- login_test.php
+++ login_test.php
@@ -2,31 +2,31 @@
 //include_once("includes/jQuery.js");
 echo '<head><script src="includes/jQuery/jquery-1.11.3.min.js"></script>';
 echo '<script language=javascript>$(document).ready(function(){
-alert("a");
+//alert("a");
 	$("#login").submit(function(){
 var data=new Object();
 data.username=$("#username").val();
 data.pass=$("#pass").val();
-alert("b");
+//alert("b");
 var options=new Object();
 options.data=data;
 options.dataType="text";
 options.type="get";
 options.success=function(responce){
-alert("f");
+//alert("f");
 $("#results").text(responce);
 };
-alert("c");
+//alert("c");
 options.url="login_ajax.php";
-alert("d");
+//alert("d");
 
 $.ajax(options);
-alert("e");
-
+//alert("e");
+return false;
 });
 	});</script></head>';
 echo '<div id="results">qqqqqqqqq</div>';
-echo '<form id="login"  method="POST">
+echo '<form id="login" action="login.php" method="POST">
                 <Label>Name:</Label><input type="text" id="username" size="20" maxlength="30" /><br>
                 <Label>Pass&nbsp;:</Label><input type="password" id="pass" size="20" maxlength="30" /><br>
         <input type="submit" id="submit" value="login">
